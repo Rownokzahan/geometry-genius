88467e73f57b86a5648f2ae9deed0c15518687d2
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-02-18 01:08:31 +0600

    add js for  triangle and area calculation

---
+++ js/script.js
@@ -0,0 +1,42 @@
+document.getElementById('calculate-triangle').addEventListener('click', function () {
+    const base = getInputValue('triangle-b');
+    const height = getInputValue('triangle-h');
+
+    if (!base || !height) {
+        return;
+    }
+
+    setAreaCalculation('Triangle', 0.5 * base * height);
+
+});
+
+function getInputValue(id) {
+    const value = parseFloat(document.getElementById(id).value);
+
+    if (isNaN(value) || value < 0) {
+        document.querySelector(`#${id}`).classList.add('border', 'border-red-700');
+        document.querySelector(`#${id} + p`).classList.remove('hidden');
+        return false;
+    }
+
+    return value;
+}
+
+
+let count = 1;
+function setAreaCalculation(name, area) {
+    area = area.toFixed(2);
+    const row = document.createElement('tr');
+
+    row.innerHTML =
+        `<tr>
+            <td>${count}.</td>
+            <td class="px-2">${name}</td>
+            <td class="pr-2">${area}cm<sup>2</sup></td>
+            <td>
+                <button class="text-white text-end px-4 py-1 bg-blue-600 rounded">Convert to m<sup>2</sup></button>
+            </td>
+        </tr>`
+    document.getElementById('calculation-table').appendChild(row);
+    count++;
+}
