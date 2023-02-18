ae85b813d6e75103ebaccfcda0ddb90432695d5f
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-02-18 12:17:19 +0600

    change shape cards background color randomly on mouse hover

--- js/script.js
+++ js/script.js
@@ -56,6 +56,20 @@ document.getElementById('ellipse-btn').addEventListener('click', function () {
     setAreaCalculation('Ellipse', 3.14 * aAxis * bAxis);
 });
 
+
+// change background color randomly on mouse over
+const shapeCards = document.getElementsByClassName('shape-card');
+for(const shapeCard of shapeCards){
+    shapeCard.addEventListener('mouseover',function(event){
+        const letters = '0123456789ABCDEF'.split('');
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        shapeCard.style.backgroundColor = color;
+    });
+}
+
 function getInputValue(id) {
     const value = parseFloat(document.getElementById(id).value);
 
