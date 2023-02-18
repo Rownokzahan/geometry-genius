cf0d4dd0e97efc748de2dabd8923df76b3eeda12
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-02-18 13:06:25 +0600

    fix responsive issues of calculation area

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,1 @@
 
-function generateRandomColor() {
-    const letters = '0123456789ABCDEF'.split('');
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-document.addEventListener("mouseover", function () {
-    document.body.style.backgroundColor = randomColor();
-});
-
-document.querySelector('circle').addEventListener('mouseover', event => {
-    event.target.style.fill = randomColor();
-});
